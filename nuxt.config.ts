ccdf44a74e1dae24a8cf366a9173752168f8d9fc
w33bvgl <w33bv.gl@gmail.com>
2026-05-31 13:46:23 +0300

    add invoice

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: false },
-
   css: ['~/assets/css/main.css'],
+  modules: ['@nuxt/icon', '@nuxtjs/color-mode'],
+
+    colorMode: {
+        classSuffix: '',
+        preference: 'light',
+        fallback: 'light',
+    },
 })
